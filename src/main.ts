fe34d19925ae2d617ffebed3eae675832bb885f5
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-06-30 21:40:38 -0700

    import theme.scss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
-// import './theme.scss';
 
 bootstrapApplication(App, appConfig);
